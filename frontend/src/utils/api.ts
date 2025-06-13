8be1a69ebd1e7bb7064fb46024ad5cece9304e5b
Ronak Gupta <93486131+ronakgupta11@users.noreply.github.com>
2025-06-13 10:43:30 +0530

    api update

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000/api/v1';
+// Use Vite's import.meta.env for environment variables
+const API_BASE_URL = 'http://ec2-13-200-253-23.ap-south-1.compute.amazonaws.com:8000/api/v1';
+// console.log('API_BASE_URL',process.env.REACT_APP_API_URL);
 
 export const generateCode = async (imageData: string) => {
   try {
